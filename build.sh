a5e78513d152bc05527664089e2d02e1b0c4109d
Rick van Rein <rick@openfortress.nl>
2018-10-24 15:28:52 +0200

    TLS Pool now works well under Python. A heroic journey into file descriptor territory, making sure that Python didn't think some had to be closed while UNIX already ditched them (and possibly re-allocated them to another process) saved a lot of spurious behaviour in any but the simplest cases :-) The wip-cherrypy work also advances along with this; the cheroot web server now works as one might expect, and cherrypy probably does, too.

--- build.sh
+++ build.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+# The following bash macro accepts numerous forms, and even defaults to
+# the current directory.  It will use the build directory name to tag the
+# image, and also tag it under arpa2.
+#
+# builddir <subdirname>
+# builddir <absdirname>
+# builddir .
+# builddir
+#
 builddir () {
-	DIR="${1%/}"
-	docker build -t "$DIR" "$DIR" && docker tag "$DIR" arpa2:"$DIR" && touch "$DIR"/.built
+	DIR=$( cd ${1:-.} ; pwd )
+	TAG=$(basename ${DIR:-.})
+	docker build -t "$TAG" "$DIR" && docker tag "$TAG" arpa2:"$TAG" && touch "$DIR"/.built
 }
 
 builddir base
@@ -17,4 +27,10 @@ builddir build-tlspool
 
 builddir demo-identityhub
 builddir demo-reservoir
+
+# Reconstruct the DOT image
+
+cd $(basename "$0")
+./deps.sh > deps.dot
+dot -T png deps.dot > deps.png
 
