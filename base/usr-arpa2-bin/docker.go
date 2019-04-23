eb737dc63d2351f2ed7e7cbdcf58e7ca420cbb11
Rick van Rein <rick@openfortress.nl>
2019-04-23 08:13:37 +0200

    Updates to init / runit  - Improved docker.go for simple use (arpa2:base)  - Prepared runit with Docker tweaks (arpa2:build-bin)  - Started rsyslogd as runit service (arpa2:build-bin)

--- base/usr-arpa2-bin/docker.go
+++ base/usr-arpa2-bin/docker.go
@@ -4,7 +4,8 @@ mkdir -p /var/arpa2
 
 uptime -s >> /var/arpa2/docker.boots
 
-if [ `wc -l < /var/arpa2/docker.boots` -eq 1 ]
+BOOTS=`wc -l < /var/arpa2/docker.boots`
+if [ $BOOTS -eq 1 ]
 then
 
 	sed -i /etc/bash.bashrc -e 's/^PS1=/# PS1=/'
@@ -13,19 +14,6 @@ then
 	then
 		# echo 'First boot, running install script'
 		/usr/arpa1/bin/docker.install
-	fi
-fi
-
-uptime -s >> /tmp/docker.runs
-
-RUNS=`wc -l < /tmp/docker.runs`
-if [ $RUNS -eq 1 ]
-then
-	if [ -r /var/arpa2/docker.hostname ]
-	then
-		HOST=`cat /var/arpa2/docker.hostname`
-		# echo "Additional host name(s) $HOST"
-		sed -i /etc/hosts -e '$s/$/ '"$HOST"'/'
 	fi
 fi
 
@@ -41,21 +29,35 @@ then
 	NAME=`cat /var/arpa2/docker.name`
 fi
 
-export PS1="$NAME$RUNS\\$ "
-# echo "Setting shell prompt to $PS1"
-
 if [ "`tty`" == "not a tty" ]
 then
 	if [ -x /usr/arpa2/bin/docker.daemon ]
 	then
 		# echo 'Jumping into docker.daemon program'
 		exec /usr/arpa2/bin/docker.daemon
-	else
-		# echo 'Looping forever'
-		echo 'Staying alive, you can use ^C but not ^Z'
-		while true ; do sleep 86400 ; done
 	fi
+	# echo 'Looping forever'
+	echo 'Staying alive, you can use ^C but not ^Z'
+	while true ; do sleep 86400 ; done
 else
+
+	uptime -s >> /tmp/docker.runs
+
+	RUNS=`wc -l < /tmp/docker.runs`
+	if [ $RUNS -eq 1 ]
+	then
+		if [ -r /var/arpa2/docker.hostname ]
+		then
+			HOST=`cat /var/arpa2/docker.hostname`
+			# echo "Additional host name(s) $HOST"
+			sed -i /etc/hosts -e '$s/$/ '"$HOST"'/'
+		fi
+	fi
+
+	export PS1="$NAME$RUNS\\$ "']0;'$NAME${BOOTS# }''
+	# echo "Setting shell prompt to $PS1 and window title to $NAME${BOOTS# }"
+	echo -n ']0;'$NAME${BOOTS# }''
+
 	if [ -x /usr/arpa2/bin/docker.interact ]
 	then
 		# echo 'Jumping into interactive program'
@@ -65,3 +67,5 @@ else
 		exec /bin/bash
 	fi
 fi
+echo 'End of docker.go'
+exit
