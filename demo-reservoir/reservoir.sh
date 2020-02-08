0a84e6752eb274135eb272df2474d461123d034b
Rick van Rein <rick@openfortress.nl>
2020-02-08 13:52:07 +0100

    Made arpa2api work again (JSON -> GSS-API -> AMQP)  - Introduced Kerberos5 keytab for the server  - Client continues to use its own credential  - Changed names accordingly to the AMQP changes on GitHub  - AMQP client/server are now called arpa2api and arpa2api.d

--- demo-reservoir/reservoir.sh
+++ demo-reservoir/reservoir.sh
@@ -9,6 +9,19 @@ echo
 #LOCKOUT# sed -i -e '$,$s/^\([^ \t]*\)[ \t]/\1 reservoir.arpa2 /' /etc/hosts
 sed -e '$,$s/^\([^ \t]*\)[ \t]/\1 reservoir.arpa2 /' < /etc/hosts > /etc/hosts2 && cp /etc/hosts2 /etc/hosts
 
+echo 'Starting OpenSSH server...'
+echo
+/etc/init.d/ssh start
+
+echo 'Starting Qpid Dispatch Router on port 5672...'
+echo
+/usr/local/sbin/qdrouterd &
+
+echo 'Starting JSON-over-AMQP service arpa2server'
+echo
+export KRB5_KTNAME=/etc/arpa2/arpa2api.keytab
+/usr/local/bin/arpa2server &
+
 echo Configuring OpenLDAP
 echo
 slapadd -c -f /etc/ldap/slapd.conf -l /root/initial.ldif
@@ -22,6 +35,14 @@ echo 'Starting OpenLDAP (IPv6 and IPv4) on port 1388 and /tmp/ldap-socket...'
 echo
 /usr/sbin/slapd -d any -h "ldapi://%2ftmp%2fldap-socket ldap://:1388/"
 RETVAL=$?
+
+echo 'Stopping JSON-over-AMQP service with arpa2server...'
+echo
+killall arpa2server
+
+echo 'Stopping Qpid Dispatch Router...'
+echo
+killall qdrouterd
 
 echo
 echo "...exit ($RETVAL) from OpenLDAP"
